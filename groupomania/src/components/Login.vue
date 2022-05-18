c9915522a7337c0328f4226413997fbae37f9abd
stevendkl <doomlight@gmail.com>
2022-05-19 00:06:35 +1000

    create login page

--- groupomania/src/components/Login.vue
+++ groupomania/src/components/Login.vue
@@ -1,6 +1,10 @@
 <template>
-    <div>
-        登录组件
+    <div class="login_container">
+        <div class="login_box">
+          <div class="avatar_box">
+            <img src="../assets/logo.svg" alt="">
+          </div>
+        </div>        
     </div>
 </template>
 
@@ -10,4 +14,41 @@ export default {}
 
 <!-- scoped定义样式只在本文件中应用 -->
 <style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 80%;
+  max-width: 450px;
+  height: 800%;
+  max-height: 400px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #ffd7d7;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+
 </style>
